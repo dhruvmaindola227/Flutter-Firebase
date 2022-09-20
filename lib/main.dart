c4c83dfd242d403abf60a091a1fa5f173f18ef23
Dhruv <maindola.dhruv@gmail.com>
2022-09-20 23:15:05 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -53,169 +53,55 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  var nameController = TextEditingController();
+  var rnoController = TextEditingController();
+  var ageController = TextEditingController();
+  var fatherController = TextEditingController();
+  var motherController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-          Padding(
-            padding: EdgeInsets.only(top : screenHeight * 0.05),
-            child: SizedBox(
-              width: double.infinity,
-              child: Text("Insert details",
-              style: GoogleFonts.poppins(
-                fontSize: 20.0
-              ),
-              textAlign: TextAlign.center,
-              ),
-            ),
-          ),
-       Padding(
-        padding: EdgeInsets.only(left: screenWidth * 0.04, top: screenHeight * 0.09),
-        child: Row(
-          children:[
-            const Text("Name : ",
-            textScaleFactor: 1.3,),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
             Padding(
-              padding: EdgeInsets.only(left : screenWidth * 0.02),
+              padding: EdgeInsets.only(top : screenHeight * 0.05),
               child: SizedBox(
-                height: screenHeight * 0.06,
-                width: screenWidth * 0.72,
-                child: Padding(
-                  padding: EdgeInsets.only(left : screenWidth * 0.03),
-                  child: const TextField(
-                      style: TextStyle(
-                        letterSpacing: 1.0
-                      ),
-                      decoration: InputDecoration(
-                        focusColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        labelText: "Enter your name",
-                      ),
-                    ),
+                width: double.infinity,
+                child: Text("Insert details",
+                style: GoogleFonts.poppins(
+                  fontSize: 20.0
+                ),
+                textAlign: TextAlign.center,
                 ),
               ),
             ),
-          ],
-        ),
-      ),
-      Padding(
-          padding: EdgeInsets.only(
-              left: screenWidth * 0.05, top: screenHeight * 0.05),
-          child: Row(
-            children: [
-              const Text(
-                "R_no : ",
-                textScaleFactor: 1.3,
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: screenWidth * 0.02),
-                child: SizedBox(
-                  height: screenHeight * 0.06,
-                  width: screenWidth * 0.73,
-                  child: Padding(
-                    padding: EdgeInsets.only(left : screenWidth * 0.04),
-                    child: const TextField(
-                      style: TextStyle(letterSpacing: 1.0),
-                      decoration: InputDecoration(
-                        focusColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        labelText: "Enter your roll number",
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-        Padding(
-          padding: EdgeInsets.only(
-              left: screenWidth * 0.05, top: screenHeight * 0.05),
-          child: Row(
-            children: [
-              const Text(
-                "Age : ",
-                textScaleFactor: 1.3,
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: screenWidth * 0.03),
-                child: SizedBox(
-                  height: screenHeight * 0.06,
-                  width: screenWidth * 0.74,
-                  child: Padding(
-                    padding: EdgeInsets.only(left : screenWidth * 0.05),
-                    child: const TextField(
-                      style: TextStyle(letterSpacing: 1.0),
-                      decoration: InputDecoration(
-                        focusColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        labelText: "Enter your age",
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ), 
-        Padding(
-          padding: EdgeInsets.only(
-              left: screenWidth * 0.05, top: screenHeight * 0.05),
-          child: Row(
-            children: [
-              const Text(
-                "Father's\nName: ",
-                textScaleFactor: 1.3,
-              ),
-              Padding(
-                padding: EdgeInsets.only(left: screenWidth * 0.03),
-                child: SizedBox(
-                  height: screenHeight * 0.06,
-                  width: screenWidth * 0.7,
-                  child: Padding(
-                    padding:  EdgeInsets.only(left : screenWidth * 0.01),
-                    child: const TextField(
-                      style: TextStyle(letterSpacing: 1.0),
-                      decoration: InputDecoration(
-                        focusColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        labelText: "Enter your father's name",
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              
-            ],
-          ),
-        ),
          Padding(
-          padding: EdgeInsets.only(
-              left: screenWidth * 0.05, top: screenHeight * 0.05),
+          padding: EdgeInsets.only(left: screenWidth * 0.04, top: screenHeight * 0.09),
           child: Row(
-            children: [
-              const Text(
-                "Mother's\nName: ",
-                textScaleFactor: 1.3,
-              ),
+            children:[
+              const Text("Name : ",
+              textScaleFactor: 1.3,),
               Padding(
-                padding: EdgeInsets.only(left: screenWidth * 0.0),
+                padding: EdgeInsets.only(left : screenWidth * 0.02),
                 child: SizedBox(
                   height: screenHeight * 0.06,
-                  width: screenWidth * 0.71,
+                  width: screenWidth * 0.72,
                   child: Padding(
-                    padding: EdgeInsets.only(left: screenWidth * 0.02),
-                    child: const TextField(
-                      style: TextStyle(letterSpacing: 1.0),
-                      decoration: InputDecoration(
-                        focusColor: Colors.white,
-                        border: OutlineInputBorder(),
-                        labelText: "Enter your mother's name",
+                    padding: EdgeInsets.only(left : screenWidth * 0.03),
+                    child: TextField(
+                      controller : nameController,
+                        style: const TextStyle(
+                          letterSpacing: 1.0
+                        ),
+                        decoration: const InputDecoration(
+                          focusColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: "Enter your name",
+                        ),
                       ),
-                    ),
                   ),
                 ),
               ),
@@ -223,30 +109,162 @@ class _MainScreenState extends State<MainScreen> {
           ),
         ),
         Padding(
-          padding: EdgeInsets.only(left : screenWidth * 0.05 , top : screenHeight * 0.08),
-          child: SizedBox(
-
-            height: 45.0,
-            width: 350.0,
-            child: ElevatedButton(onPressed: () {
-                
-            },            
-            style: const ButtonStyle(
-              backgroundColor: MaterialStatePropertyAll(ConstantColors.darkBlue),
-                textStyle: MaterialStatePropertyAll(
-                  TextStyle(
-                    fontSize: 18.0,
-                    color: Colors.white60
-                    )
+            padding: EdgeInsets.only(
+                left: screenWidth * 0.05, top: screenHeight * 0.05),
+            child: Row(
+              children: [
+                const Text(
+                  "R_no : ",
+                  textScaleFactor: 1.3,
                 ),
-                overlayColor: MaterialStatePropertyAll(Colors.blue),
-                shadowColor: MaterialStatePropertyAll(Colors.black),
-                elevation: MaterialStatePropertyAll(12)
-            ), child: const Text("SUBMIT"),
+                Padding(
+                  padding: EdgeInsets.only(left: screenWidth * 0.02),
+                  child: SizedBox(
+                    height: screenHeight * 0.06,
+                    width: screenWidth * 0.73,
+                    child: Padding(
+                      padding: EdgeInsets.only(left : screenWidth * 0.04),
+                      child: TextField(
+                        controller: rnoController,
+                        style: const TextStyle(letterSpacing: 1.0),
+                        decoration: const InputDecoration(
+                          focusColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: "Enter your roll number",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        )
-      ],
+          Padding(
+            padding: EdgeInsets.only(
+                left: screenWidth * 0.05, top: screenHeight * 0.05),
+            child: Row(
+              children: [
+                const Text(
+                  "Age : ",
+                  textScaleFactor: 1.3,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: screenWidth * 0.03),
+                  child: SizedBox(
+                    height: screenHeight * 0.06,
+                    width: screenWidth * 0.74,
+                    child: Padding(
+                      padding: EdgeInsets.only(left : screenWidth * 0.05),
+                      child:  TextField(
+                        controller: ageController,
+                        style: const TextStyle(letterSpacing: 1.0),
+                        decoration: const InputDecoration(
+                          focusColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: "Enter your age",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ), 
+          Padding(
+            padding: EdgeInsets.only(
+                left: screenWidth * 0.05, top: screenHeight * 0.05),
+            child: Row(
+              children: [
+                const Text(
+                  "Father's\nName: ",
+                  textScaleFactor: 1.3,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: screenWidth * 0.03),
+                  child: SizedBox(
+                    height: screenHeight * 0.06,
+                    width: screenWidth * 0.7,
+                    child: Padding(
+                      padding:  EdgeInsets.only(left : screenWidth * 0.01),
+                      child:  TextField(
+                        controller: fatherController,
+                        style: const TextStyle(letterSpacing: 1.0),
+                        decoration: const InputDecoration(
+                          focusColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: "Enter your father's name",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                
+              ],
+            ),
+          ),
+           Padding(
+            padding: EdgeInsets.only(
+                left: screenWidth * 0.05, top: screenHeight * 0.05),
+            child: Row(
+              children: [
+                const Text(
+                  "Mother's\nName: ",
+                  textScaleFactor: 1.3,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: screenWidth * 0.0),
+                  child: SizedBox(
+                    height: screenHeight * 0.06,
+                    width: screenWidth * 0.71,
+                    child: Padding(
+                      padding: EdgeInsets.only(left: screenWidth * 0.02),
+                      child: TextField(
+                        controller: motherController,
+                        style:const  TextStyle(letterSpacing: 1.0),
+                        decoration: const InputDecoration(
+                          focusColor: Colors.white,
+                          border: OutlineInputBorder(),
+                          labelText: "Enter your mother's name",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left : screenWidth * 0.05 , top : screenHeight * 0.08),
+            child: SizedBox(
+    
+              height: 45.0,
+              width: 350.0,
+              child: ElevatedButton(onPressed: () {
+                  Map<String , String> map = Map();
+                  map["name"] = nameController.text;
+                  map["rno"] = rnoController.text;
+                  map["age"] = ageController.text;
+                  map["father"] = fatherController.text;
+                  map["mother"] = motherController.text;
+                  print(map);
+              },            
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(ConstantColors.darkBlue),
+                  textStyle: MaterialStatePropertyAll(
+                    TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white60
+                      )
+                  ),
+                  overlayColor: MaterialStatePropertyAll(Colors.blue),
+                  shadowColor: MaterialStatePropertyAll(Colors.black),
+                  elevation: MaterialStatePropertyAll(12)
+              ), child: const Text("SUBMIT"),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
