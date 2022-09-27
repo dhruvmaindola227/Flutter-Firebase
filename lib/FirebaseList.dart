85624698307100adcd877bb70465ed4c939c5a66
Dhruv <maindola.dhruv@gmail.com>
2022-09-27 20:03:21 +0530

    read operation firebase working

--- lib/FirebaseList.dart
+++ lib/FirebaseList.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:new_app/constants_color.dart';
+import 'package:new_app/main.dart';
 
 class FireBaseList extends StatefulWidget {
   const FireBaseList({super.key});
@@ -14,10 +15,7 @@ class _FireBaseListState extends State<FireBaseList> {
       FirebaseFirestore.instance.collection('users');
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ListOfItems(),
-    );
+    return const ListOfItems();
   }
 }
 
@@ -29,6 +27,8 @@ class ListOfItems extends StatefulWidget {
 }
 
 class _ListOfItemsState extends State<ListOfItems> {
+  final CollectionReference _users = FirebaseFirestore.instance.collection('users');
+  late AsyncSnapshot<QuerySnapshot> streamSnapshot;
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
@@ -38,6 +38,31 @@ class _ListOfItemsState extends State<ListOfItems> {
         title: const Text("Firebase List"),
         backgroundColor: ConstantColors.darkBlue,
       ),
+      body: StreamBuilder(builder: (context, snapshot) { //snapshot has the data and the docs in it.
+        if(snapshot.hasData){
+          return ListView.builder(
+                itemCount: snapshot.data!.docs.length,  //number of rows in firebase
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot = 
+                  snapshot.data!.docs[index];
+            return Card(
+              margin: const EdgeInsets.all(10),
+              child: ListTile(
+                title: Text(documentSnapshot['age'].toString()),
+                subtitle: Text(documentSnapshot['name'].toString())
+              ),
+            );
+          },
+          );
+        }else {
+          return const Text("no data");
+        }
+      },
+      stream: _users.snapshots(
+        includeMetadataChanges: false
+        ),
+        )
+
     );
   }
 }
