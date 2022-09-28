f6c887001e9c9b441dbd36917afa464358d6aeb3
Dhruv <maindola.dhruv@gmail.com>
2022-09-28 13:38:21 +0530

    ui rendered properly

--- lib/FirebaseList.dart
+++ lib/FirebaseList.dart
@@ -27,79 +27,84 @@ class ListOfItems extends StatefulWidget {
 }
 
 class _ListOfItemsState extends State<ListOfItems> {
-  final CollectionReference _users = FirebaseFirestore.instance.collection('users');
-  late AsyncSnapshot<QuerySnapshot> streamSnapshot;
+  final CollectionReference _users =
+      FirebaseFirestore.instance.collection('users');
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Firebase List"),
-        backgroundColor: ConstantColors.darkBlue,
-      ),
-      body: StreamBuilder(builder: (context, snapshot) { //snapshot has the data and the docs(rows) in it.
-        if(snapshot.hasData){
-          return ListView.builder(
-                itemCount: snapshot.data!.docs.length,  //number of rows in firebase
+        appBar: AppBar(
+          title: const Text("Firebase List"),
+          backgroundColor: ConstantColors.darkBlue,
+        ),
+        body: StreamBuilder(
+          builder: (context, snapshot) {
+            //snapshot has the data and the docs(rows) in it.
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount:
+                    snapshot.data!.docs.length, //number of rows in firebase
                 itemBuilder: (context, index) {
-                  final DocumentSnapshot documentSnapshot = 
-                  snapshot.data!.docs[index];
-            return Card(
-              margin: const EdgeInsets.all(10),
-              elevation: 10,
-              child: ColoredBox(              
-                color: ConstantColors.darkBlue,
-                child: SizedBox(
-                  height: screenHeight * 0.1,
-                  width: screenWidth * 0.9,
-                  child: Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left : 10.0 , top : 20.0),
-                        child: Column(
-                           crossAxisAlignment: CrossAxisAlignment.start,
+                  final DocumentSnapshot documentSnapshot =
+                      snapshot.data!.docs[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SizedBox(
+                      height: screenHeight * 0.1,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(
+                            10,
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text("Name : ${documentSnapshot['name'].toString()}"),
                             Padding(
-                              padding: const EdgeInsets.only(top : 8.0),
-                              child: Text("Roll Number : ${documentSnapshot['rno'].toString()}"),
+                              padding:
+                                  const EdgeInsets.only(left: 10.0, top: 20.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                      "Name : ${documentSnapshot['name'].toString()}"),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 8.0),
+                                    child: Text(
+                                        "Roll Number : ${documentSnapshot['rno'].toString()}"),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(right: 10.0, top: 15.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.end,
+                                children: [
+                                  Text(
+                                      "Age : ${documentSnapshot['age'].toString()}"),
+                                  Text(
+                                      "Mother name : ${documentSnapshot['mothername'].toString()}"),
+                                  Text(
+                                      "Father name : ${documentSnapshot['fathername'].toString()}")
+                                ],
+                              ),
                             )
                           ],
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left : 120.0 , top : 15.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            Text("Age : ${documentSnapshot['age'].toString()}"),
-                            Text("Mother name : ${documentSnapshot['mothername'].toString()}"),
-                            Text( "Father name : ${documentSnapshot['fathername'].toString()}")
-                          ],
-                        ),
-                      )
-                    ],
-                  )
-              
-                ),
-              )
-              //  ListTile(
-              //   title: Text(documentSnapshot['age'].toString()),
-              //   subtitle: Text(documentSnapshot['name'].toString())
-              // ),
-            );
+                    ),
+                  );
+                },
+              );
+            } else {
+              return const Text("no data");
+            }
           },
-          );
-        }else {
-          return const Text("no data");
-        }
-      },
-      stream: _users.snapshots(
-        includeMetadataChanges: false
-        ),
-        )
-
-    );
+          stream: _users.snapshots(includeMetadataChanges: true),
+        ));
   }
 }
